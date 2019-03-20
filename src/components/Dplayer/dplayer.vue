571920c697914fe4812a23d17a3fc84fb72715d1
tianjianen <tianjianen@testin.cn>
2019-03-20 16:25:50 +0800

    add TV show

---
+++ src/components/Dplayer/dplayer.vue
@@ -0,0 +1,105 @@
+<template>
+  <div :id="playerName" class="vchat-dplayer"></div>
+</template>
+
+<script>
+import "dplayer/dist/DPlayer.min.css";
+import Hls from "hls.js";
+import DPlayer from "dplayer";
+export default {
+  name: "dplayer",
+  props: ["videoInfo"],
+  data() {
+    return {
+      dp: null,
+      hls: null
+    };
+  },
+  watch: {
+    "videoInfo.src": {
+      // 切换视频
+      handler(src) {
+        if (this.videoInfo.type === "hls") {
+          if (this.hls) {
+            this.hls.destroy();
+          }
+          this.dp.switchVideo({
+            url: src,
+            type: "customHls",
+            customType: {
+              customHls: (video, player) => {
+                this.hls = new Hls();
+                this.hls.loadSource(video.src);
+                this.hls.attachMedia(video);
+              }
+            }
+          });
+          this.dp.on("canplaythrough", () => {
+            this.dp.play();
+          });
+        }
+      }
+    }
+  },
+  beforeDestroy() {
+    // 离开页面销毁播放器
+    if (this.hls) {
+      this.hls.destroy();
+    }
+    this.dp.destroy();
+  },
+  computed: {
+    playerName() {
+      // 随机id
+      return "dplayer-" + Math.random();
+    }
+  },
+  methods: {
+    initHlsPlayer() {
+      this.dp = new DPlayer({
+        container: document.getElementById(this.playerName),
+        autoplay: this.videoInfo.autoplay || false,
+        video: {
+          url: this.videoInfo.src,
+          type: "customHls",
+          customType: {
+            customHls: (video, player) => {
+              this.hls = new Hls();
+              this.hls.loadSource(video.src);
+              this.hls.attachMedia(video);
+            }
+          }
+        }
+      });
+    },
+    initMp4Player() {
+      this.dp = new DPlayer({
+        container: document.getElementById(this.playerName),
+        autoplay: this.videoInfo.autoplay || false,
+        preload: "none",
+        video: {
+          url: this.videoInfo.src,
+          type: "auto",
+          pic: this.videoInfo.pic
+        },
+        mutex: true
+      });
+    }
+  },
+  mounted() {
+    this.$nextTick(_ => {
+      if (this.videoInfo.type === "hls") {
+        this.initHlsPlayer();
+      } else if (this.videoInfo.type === "mp4") {
+        this.initMp4Player();
+      }
+    });
+  }
+};
+</script>
+
+<style >
+.vchat-dplayer {
+  width: 100%;
+}
+</style>
--- src/components/pictureinfo/pictureinfo.vue
+++ src/components/pictureinfo/pictureinfo.vue
@@ -1,28 +1,124 @@
 <template>
-	<div class="picture">
-		<img width="100%" :src="images" alt="">
-		<!-- <Uploader type="images" dataType="url" :files="images" readonly></Uploader> -->
- 
-	</div>
+  <div class="picture">
+    <img width="100%" :src="images" alt>
+    <Row>
+      <Cell>
+        <SwitchList v-model="currVideo" :datas="videoList" @change="showinfo">
+          </SwitchList>
+      </Cell>
+    </Row>
+    
+    <dplayer :videoInfo="videoInfo"></dplayer>
+  </div>
 </template>
 <script>
+import dplayer from "../Dplayer/dplayer";
 export default {
-	data() {
-		return {
-			images: ''
-		}
-	},
-	methods:{
-		_getPicture() {
-			this.images = 'http://bing.getlove.cn/bingImage'	
-		}
-	},
-	created() {
-		this._getPicture();
-	}	
-}
+  name: "videoGallery",
+  data() {
+    return {
+      images: "",
+      currVideo: 1,
+      activeName: "1",
+      activeMenu: 0,
+      loading: false,
+      videoList: [
+        {
+          key:1,
+          title: "CCTV-1",
+          url: "http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8"
+        },
+        {
+          key:2,
+          title: "CCTV-5",
+          url: "http://ivi.bupt.edu.cn/hls/cctv5hd.m3u8"
+        },
+        {
+          key:3,
+          title: "CCTV-6",
+          url: "http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8"
+        },
+        {
+          key:4,
+          title: "CCTV-3",
+          url: "http://ivi.bupt.edu.cn/hls/cctv3hd.m3u8"
+        },
+        {
+          key:5,
+          title: "CCTV-8",
+          url: "http://ivi.bupt.edu.cn/hls/cctv8hd.m3u8"
+        },
+        {
+          key:6,
+          title: "北京卫视",
+          url: "http://ivi.bupt.edu.cn/hls/btv1hd.m3u8"
+        },
+        {
+          key:7,
+          title: "湖南卫视",
+          url: "http://ivi.bupt.edu.cn/hls/hunanhd.m3u8"
+        },
+        {
+          key:8,
+          title: "浙江卫视",
+          url: "http://ivi.bupt.edu.cn/hls/zjhd.m3u8"
+        },
+        {
+          key:9,
+          title: "江苏卫视",
+          url: "http://ivi.bupt.edu.cn/hls/jshd.m3u8"
+        },
+        {
+          key:10,
+          title: "东方卫视",
+          url: "http://ivi.bupt.edu.cn/hls/dfhd.m3u8"
+        },
+        {
+          key:11,
+          title: "辽宁卫视",
+          url: "http://ivi.bupt.edu.cn/hls/lnhd.m3u8"
+        },
+         {
+          key:12,
+          title: "卡酷少儿",
+          url: "http://ivi.bupt.edu.cn/hls/btv10.m3u8"
+        }
+      ]
+    };
+  },
+  computed: {
+    videoInfo() {
+      return {
+        src: this.videoList[this.activeMenu].url,
+        type: "hls",
+        autoplay: true
+      };
+    }
+  },
+  components: {
+    dplayer
+  },
+  methods: {
+    _getPicture() {
+      this.images = "http://bing.getlove.cn/bingImage";
+    },
+    showinfo(val) {
+      console.log(val);
+      this.activeMenu = Number(val.key-1);
+    }
+  },
+  created() {
+    this._getPicture();
+  },
+ 
+};
 </script>
-<style scoped>
-
+<style >
+.h-switchlist>span{
+  margin: 5px 10px;
+}
+.h-switchlist>span:first-child{
+  margin-left: 0px;
+}
 </style>
 
